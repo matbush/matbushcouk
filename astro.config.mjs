0d9f89ab7726a212e96af42f606497d5c5531e55
Matt Bush <matjbush@gmail.com>
2025-08-09 16:31:14 +0100

    Update astro, add speed insights, update current tech lead site order & links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    adapter: vercel({ analytics: true }),
+    adapter: vercel({ webAnalytics: true, maxDuration: 8 }),
 });
